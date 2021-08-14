116559720876d7bea166af4827e856b501eee6e2
H1ako <nikita-sobolev-wd@yandex.ru>
2021-08-15 01:29:13 +0400

    end of installation

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/php_project_in_use/open_cart_test/admin/');
+define('HTTP_CATALOG', 'http://localhost/php_project_in_use/open_cart_test/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/php_project_in_use/open_cart_test/admin/');
+define('HTTPS_CATALOG', 'http://localhost/php_project_in_use/open_cart_test/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Server/data/htdocs/php_project_in_use/open_cart_test/admin/');
+define('DIR_SYSTEM', 'C:/Server/data/htdocs/php_project_in_use/open_cart_test/system/');
+define('DIR_IMAGE', 'C:/Server/data/htdocs/php_project_in_use/open_cart_test/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'C:/Server/data/htdocs/php_project_in_use/open_cart_test/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'site_op_test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
